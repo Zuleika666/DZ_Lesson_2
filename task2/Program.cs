1ea7b1b77dd29e1494dca4a3c8f47d17f30b32f6
Anna <annahuanna666666@gmail.com>
2023-01-04 17:36:19 +0300

    решено задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая вывдит случайное трехзначное число и удаляет вторую цифру этого числа 
+Нпример 456 -> 46
+*/
+
+Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end +1);
+    return number;
+}
+
+int DeleteSecondDigit(int number)
+{
+    int dig3 = number % 10;
+    int dig1 = number / 100;
+    int digits = dig1 * 10 + dig3;
+    return digits;
+}
+
+int randomNumber = GetNumberFromRange(100, 999);
+int digits2 = DeleteSecondDigit(randomNumber);
+Console.WriteLine($"Из числа {randomNumber} получилось {digits2}");
