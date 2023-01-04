789abc22f67db6e3bb62beff141c228117a7decf
Anna <annahuanna666666@gmail.com>
2023-01-04 18:23:14 +0300

    Решена задача 19 ДЗ

---
+++ dz_zadacha19/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого
+
+Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end +1);
+    return number;
+}
+
+int SecondDigit(int number)
+{
+    int dig2 = (number % 100) / 10;
+    return dig2;
+}
+
+int random_num = GetNumberFromRange(100, 999);
+int digit = SecondDigit(random_num);
+Console.WriteLine($"Второй цифрой числа {random_num} является {digit}");
