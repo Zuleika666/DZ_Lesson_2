517f7471f51c570514d65d0877331b89e4421fcd
Anna <annahuanna666666@gmail.com>
2023-01-04 17:21:12 +0300

    Выполнено задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу которая выводит случайное числоиз отрезка [10, 99] и показывает наибоьшую цифру числа
+// Например 78 > 8
+// 12 > 2,  85 > 8
+
+Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end +1);
+    return number;
+}
+
+int GetMaxDigit(int number)
+{
+    int max = number % 10;
+    int dig2 = number / 10;
+    if (max < dig2)
+    {max = dig2;}
+    
+    return max;
+}
+
+int randomNumber = GetNumberFromRange(10, 99);
+int maxDigit = GetMaxDigit(randomNumber);
+Console.WriteLine($"Для числа {randomNumber} максимальной цифрой явл {maxDigit}");
